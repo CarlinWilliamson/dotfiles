61db6d6969b4f5ff91affcdb1faeb4f8d34ff98f
Carlin Williamson <carlin.williamson1@gmail.com>
2021-09-03 21:58:32 +1000

    install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+files="gitconfig inputrc bashrc config/i3"
+
+for file in $files; do
+    target="$(pwd)/$file"
+    link_name="$HOME/.$file"
+
+    if [ $(echo "$file" | grep \/) ]
+    then
+        link_name="$(echo "$link_name" | sed -E "s/(.*)\/.*/\1/")"
+    fi
+        
+    ln -s -i -v "$target" "$link_name"
+done
+
+. ~/.bashrc
