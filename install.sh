67d2e24cdfb856b98cb81b266cc6c32c39e883ad
Carlin Williamson <carlin.williamson1@gmail.com>
2024-10-26 22:17:06 +1100

    more generic install script

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-files="gitconfig inputrc bashrc bash_aliases vimrc config/i3 config/htop tmux.conf"
 
-for file in $files; do
-    target="$(pwd)/$file"
-    link_name="$HOME/.$file"
+if [ $# -ne 0 ]; then
+    cat << EOF
+Creates symlinks as described in links.csv
+Will prompt you if files already exist
 
-    if [ $(echo "$file" | grep \/) ]
-    then
-        link_name="$(echo "$link_name" | sed -E "s/(.*)\/.*/\1/")"
+Usage: $0
+EOF
+exit 1
+fi
+
+for link in $(tail -n +2 links.csv); do
+    target="$(pwd)/$(echo $link | cut -d ',' -f1)"
+    link_name="$HOME/$(echo $link | cut -d ',' -f2)"
+
+    if [ -L $link_name ]; then
+        if [ "$(readlink $link_name)" = $target ]; then
+            echo "$target -> $link_name already exists"
+            continue
+        fi
     fi
-        
+
     ln -s -i -v "$target" "$link_name"
 done
 
